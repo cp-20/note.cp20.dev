803220ec2034d1fbe2743d10c1e8e5a2bf08958b
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-02-03 14:18:26 +0900

    TagListの表示をなくした

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,12 +15,7 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
