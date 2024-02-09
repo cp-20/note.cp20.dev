adfe817138bf102867b3194686c46f4fd1edc679
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-02-09 23:33:39 +0900

    mobileの方でもエクスプローラーを表示するように

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,6 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.MobileOnly(Component.Explorer()),
   ],
 }
 
